e5fc85bdc1993f4b3c1908625891a75f6403bd1e
Tauseef patel <pateltauseef413@gmail.com>
2023-08-20 13:39:15 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+
+        let count = 0;
+        let saveEl = document.getElementById("save-el");
+        let countEl = document.getElementById("count-el");
+
+        function increment() {
+            count += 1;
+            countEl.textContent = count;
+        }
+
+        function save() {
+            let countStr = count + " - ";
+            saveEl.textContent += countStr;
+            countEl.textContent = 0;
+            count = 0;
+        }
+        function reset() {
+            count = 0;
+            countEl.textContent = count;
+            saveEl.textContent = "previous entries";
+            saveEl.textContent = "Values reset";     
+         }
+
+        console.log("Let's count people on the subway!");
+ 
